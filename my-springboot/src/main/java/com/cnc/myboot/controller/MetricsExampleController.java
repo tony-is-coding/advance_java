6137074a497892050b5f8765fab5e53c3813ae01
newbiwtan@163.com <tzy13726067856>
2021-08-02 22:40:36 +0800

    commit

---
+++ my-springboot/src/main/java/com/cnc/myboot/controller/MetricsExampleController.java
@@ -0,0 +1,16 @@
+package com.cnc.myboot.controller;
+
+
+/**
+ *
+ * #TODO: 研究SpringBoot的指标接入以及指标度量等
+ *
+ * 指标度量带来的好处:
+ * 1. 监控系统的稳定性
+ * 2. 多维度指标分析系统进行优化提升
+ *
+ *
+ */
+
+public class MetricsExampleController{
+}
